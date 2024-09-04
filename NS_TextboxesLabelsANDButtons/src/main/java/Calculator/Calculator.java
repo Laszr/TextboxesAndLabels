9fce72cc181737cff3f99122311c72ed9c0e0947
Laszr <vastefortnite@gmail.com>
2024-09-04 10:22:53 -0400

    calculator

--- NS_TextboxesLabelsANDButtons/src/main/java/Calculator/Calculator.java
+++ NS_TextboxesLabelsANDButtons/src/main/java/Calculator/Calculator.java
@@ -1,4 +1,4 @@
-package com.example.ns_textboxeslabelsandbuttons;
+package Calculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Calculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(com.example.ns_textboxeslabelsandbuttons.HelloApplication.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Calculator.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
