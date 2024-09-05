b8ca7b1a11c8b4504c58699b89ee7f4e89c464c4
Laszr <vastefortnite@gmail.com>
2024-09-05 08:34:15 -0400

    a

---
+++ Calculator/src/main/java/com/example/calculator/CalculatorThing.java
@@ -0,0 +1,59 @@
+package com.example.calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CalculatorThing {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    void appendNumber(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void selectOperator(ActionEvent event)
+    {
+
+    }
+    void calculate(ActionEvent event)
+    {
+        Label result_lbl = null;
+        double secondNum = Double.parseDouble(result_lbl.getText());
+        double firstNum = 0;
+        boolean isNumber;
+        Label running_lbl = null;
+        String operator = null;
+        running_lbl.setText(firstNum + " " + operator + " " + secondNum);
+        switch (operator) {
+            case "+":
+                firstNum += secondNum;
+                break;
+            case "-":
+                firstNum -= secondNum;
+                break;
+            case "*":
+                firstNum *= secondNum;
+                break;
+            case "/":
+                if (secondNum != 0)
+                {
+                    firstNum /= secondNum;
+                }
+                else
+                {
+                    result_lbl.setText(("Error 404 Nononono dont do tis"));
+                }
+                break;
+        }
+
+    }
+}
